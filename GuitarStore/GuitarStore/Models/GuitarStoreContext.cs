bcf7124f967568329584aa3528fd649253d0e6f7
Igor Yanchynskiy <59307393+Icorcka@users.noreply.github.com>
2020-10-03 11:54:21 +0300

    New Project

---
+++ GuitarStore/GuitarStore/Models/GuitarStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GuitarStore.Models
+{
+    public class GuitarStoreContext: DbContext
+    {
+        public DbSet<Guitar> Guitars { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public GuitarStoreContext(DbContextOptions<GuitarStoreContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ GuitarStore/GuitarStore/Models/SampleData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuitarStore.Models
+{
+    public class SampleData
+    {
+        public static void Initialize(GuitarStoreContext context)
+        {
+            if (!context.Guitars.Any())
+            {
+                context.Guitars.AddRange(
+                    new Guitar
+                    {
+                        Name = "Ibanez GRG 140 SB",
+                        Producer = "Ibanez",
+                        Type= "Electric Guitar",
+                        Price = 7000
+                    },
+                    new Guitar
+                    {
+                        Name = "Cort AD810",
+                        Producer = "Cort",
+                        Type= "Acoustic guitar",
+                        Price = 3000
+                    },
+                    new Guitar
+                    {
+                        Name = "Yamaha TRBX-174",
+                        Producer = "Yamaha",
+                        Type= "Bass guitar",
+                        Price = 7500
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
