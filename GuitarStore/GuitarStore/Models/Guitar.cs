2453998c3ef06b99584accca55201c4230a8c582
Igor Yanchynskiy <59307393+Icorcka@users.noreply.github.com>
2020-10-03 11:31:51 +0300

    Add files via upload

---
+++ GuitarStore/GuitarStore/Models/Guitar.cs
@@ -0,0 +1,11 @@
+﻿namespace GuitarStore.Models
+{
+    public class Guitar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Producer { get; set;}
+        public string Type { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ GuitarStore/GuitarStore/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace GuitarStore.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string User { get; set; }
+        public string Address { get; set; }
+        public string ContactPhone { get; set; }
+
+        public int GuitarId { get; set; }
+        public Guitar Guitar { get; set; }
+    }
+}
